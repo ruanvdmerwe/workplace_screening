d217ca42ce5005f45ad8b9076b93cd1ef2008047
Ruan van der Merwe <ruanvdmerwe@outlook.com>
2020-06-05 10:06:16 +0200

    chagend test file to allow any number of models to be called, instead of allways testing both recognize and detection

--- test.py
+++ test.py
@@ -9,24 +9,26 @@ parser = argparse.ArgumentParser(description='Run and test the facial recogntion
 
 parser.add_argument('-m',
                     '--mask',
-                    required=True,
+                    #required=True,
                     help='location of mask detection model')
 parser.add_argument('-f',
                     '--faces',
-                    required=True,
+                    #required=True,
                     help='location of the face encodings pkl file')
 parser.add_argument('-e',
                     '--embedding',
-                    required=True,
+                    #required=True,
                     help='location of the face embeddings model')
 parser.add_argument('-t',
                     '--tolerance',
                     nargs='?',
                     const = 0.35,
+                    default=0.35,
                     help='tolerance to match a face to known encodings')
 parser.add_argument('--probability',
                     nargs='?',
                     const = 0.7,
+                    default=0.7,
                     help='minimum probability of face to detect a face')
 parser.add_argument('-n',
                     '--known',
@@ -34,8 +36,6 @@ parser.add_argument('-n',
 parser.add_argument('-p',
                     '--person',
                     help='name of the new person to add to face encodings')
-parser.add_argument('--test',
-                    help='folder to test images')
 parser.add_argument('-l',
                     '--livestream',
                     nargs='?',
@@ -67,7 +67,6 @@ faces_folder = args['known']
 person_name = args['person']
 tolerance = float(args['tolerance'])
 face_probability = float(args['probability'])
-test = args['test']
 livestream = args['livestream']
 voice = args['voice']
 online = args['voice']
@@ -94,41 +93,24 @@ else:
     online = False
 
 if __name__ == '__main__':
-
-    # intiliaze all of the models
-    face_mask_detector = FaceMaskDetector(mask_detect_model=mask_detect_model)
-    face_embedding = FaceIdentifyDataCreation(encodings_location=encodings_location,
-                                    embeding_model_location=embeding_model_location)
-
-
-    # ----------------- run tests on previous images -----------------
-    if not test is None:
-        from imutils import paths
-        images = list(paths.list_images(test))
-        for image in images:
-            print(f"Busy with image {image}")
-            face_mask_detector.load_image_from_file(image)
-            face_mask_detector.detect_faces()
-            face_mask_detector.detect_facemask()
-            face_mask_detector.draw_boxes_around_faces()
-            face_mask_detector.display_predictions()
-
-            face_recognizer.load_image_from_file(image)
-            face_recognizer.detect_faces(face_size=(160,160))
-            face_recognizer.recognize_faces(tolerance=0.35)
-            face_recognizer.draw_boxes_around_faces()
-            face_recognizer.display_predictions()
-
+        
     # ----------------- detecting if you are wearing a face mask -----------------
-    face_mask_detector.start_video_stream()
-    if livestream:
-        face_mask_detector.capture_frame_and_detect_facemask_live(verbose=verbose)
+    if mask_detect_model is None:
+        pass
     else:
-        face_mask_detector.capture_frame_and_detect_facemask(verbose=verbose)
-        face_mask_detector.display_predictions()
+            
+        face_mask_detector = FaceMaskDetector(mask_detect_model=mask_detect_model)
+        face_mask_detector.start_video_stream()
+        if livestream:
+            face_mask_detector.capture_frame_and_detect_facemask_live(verbose=verbose)
+        else:
+            face_mask_detector.capture_frame_and_detect_facemask(verbose=verbose)
+            face_mask_detector.display_predictions()
 
     # # ----------------- Adding a new face to faces dataset -----------------
     if not faces_folder is None:
+        face_embedding = FaceIdentifyDataCreation(encodings_location=encodings_location,
+                                                embeding_model_location=embeding_model_location)
         if not person_name is None:
             face_embedding.start_video_stream()
             print("Adding new face pictures")
@@ -138,16 +120,19 @@ if __name__ == '__main__':
 
 
     # ----------------- See if face is recognized -----------------
-    print("Loading face embed model")
-    face_recognizer = FaceIdentifier(encodings_location=encodings_location,
-                                    embeding_model_location=embeding_model_location)
-    print("Starting to recognize face")
-    face_recognizer.start_video_stream()
-    if livestream:
-        face_recognizer.capture_frame_and_recognize_faces_live(tolerance=0.35,verbose=verbose)
+    if embeding_model_location is None:
+        pass
     else:
-        face_recognizer.capture_frame_and_recognize_faces(tolerance=tolerance, face_probability=face_probability,verbose=verbose)
-        face_recognizer.display_predictions()
+        print("Loading face embed model")
+        face_recognizer = FaceIdentifier(encodings_location=encodings_location,
+                                         embeding_model_location=embeding_model_location)
+        print("Starting to recognize face")
+        face_recognizer.start_video_stream()
+        if livestream:
+            face_recognizer.capture_frame_and_recognize_faces_live(tolerance=0.35,verbose=verbose)
+        else:
+            face_recognizer.capture_frame_and_recognize_faces(tolerance=tolerance, face_probability=face_probability,verbose=verbose)
+            face_recognizer.display_predictions()
 
     # ----------------- Test a few phrases -----------------
     # just run a few voice tests
@@ -157,4 +142,3 @@ if __name__ == '__main__':
         for i in range(5):
             print("Please speak")
             print(f"You said {speech_to_text.listen_and_predict(online=online)}")
-
