4d316018e37ea1d499d747c1f7e0541860c2b831
Petrus Janse van Rensburg <petrus.jvrensburg@gmail.com>
2020-06-12 12:29:08 +0200

    minor cleanup using autopep8

--- workplace_screening/gui.py
+++ workplace_screening/gui.py
@@ -14,6 +14,7 @@ import numpy as np
 import pickle
 from voice_recognition.voice_recognition import SpeechToText
 
+
 def add_text_to_image(image, text):
     """
     Will ad text to image to display in GUI. Depending on length of text,
@@ -29,29 +30,29 @@ def add_text_to_image(image, text):
         opencv image instance
     """
 
-    font = cv2.FONT_HERSHEY_SIMPLEX 
+    font = cv2.FONT_HERSHEY_SIMPLEX
     fontScale = 0.7
-    color = (255, 0, 0) 
+    color = (255, 0, 0)
     thickness = 1
     height, width, dim = image.shape
-    org = (20, height-50) 
+    org = (20, height-50)
     number_of_characters = len(text)
 
-    
-    if number_of_characters<42:
-       image = cv2.rectangle(image, (0, height), (0 + width, height-50-1*25 - 25), (0,0,0), -1)
-       return cv2.putText(image, text, org, font, fontScale, color, thickness, cv2.LINE_AA) 
+    if number_of_characters < 42:
+        image = cv2.rectangle(image, (0, height), (0 + width, height-50-1*25 - 25), (0, 0, 0), -1)
+        return cv2.putText(image, text, org, font, fontScale, color, thickness, cv2.LINE_AA)
     else:
         number_of_sentences = math.ceil(number_of_characters/42)
         words_split = text.split()
         sentences = np.array_split(words_split, number_of_sentences)
-        image = cv2.rectangle(image, (0, height), (0 + width, height-50-len(sentences)*25 - 25), (0,0,0), -1)
+        image = cv2.rectangle(image, (0, height), (0 + width, height-50-len(sentences)*25 - 25), (0, 0, 0), -1)
 
         for i, sentence in enumerate(reversed(sentences)):
             sentence = " ".join(sentence)
             org = (20, height-50-i*25)
-            image = cv2.putText(image, sentence, org, font, fontScale, color, thickness, cv2.LINE_AA) 
+            image = cv2.putText(image, sentence, org, font, fontScale, color, thickness, cv2.LINE_AA)
     return image
+
 
 class WorkPlaceScreening(object):
     def __init__(self, vs):
@@ -74,51 +75,53 @@ class WorkPlaceScreening(object):
         self.root.wm_title("Workplace Screening")
         self.root.wm_protocol("WM_DELETE_WINDOW", self.onClose)
 
-
     def videoLoop(self):
-            frame_counter = 0
-            text = "STOP! We need to check your mask, temperature and symptoms before you enter."
+        frame_counter = 0
+        text = "STOP! We need to check your mask, temperature and symptoms before you enter."
 
-            while not self.stopEvent.is_set():
+        while not self.stopEvent.is_set():
 
+            try:
+                with open('./workplace_screening/state.pkl', 'rb') as myfile:
+                    text = pickle.load(myfile)
+            except:
+                pass
+
+            if frame_counter == 5:
                 try:
-                    with open ('./workplace_screening/state.pkl', 'rb') as myfile:  
-                        text = pickle.load(myfile)
+                    with open('./workplace_screening/frame.pkl', 'wb') as filetowrite:
+                        pickle.dump(self.frame, filetowrite)
                 except:
                     pass
-                
-                if frame_counter == 5:
-                    try:
-                        with open('./workplace_screening/frame.pkl', 'wb') as filetowrite:
-                            pickle.dump(self.frame, filetowrite)                        
-                    except:
-                        pass
-                    frame_counter = 0
-                    
-                self.frame = self.vs.read()
-                self.frame = resize(self.frame, width=600)
-                image = cv2.flip(cv2.cvtColor(self.frame, cv2.COLOR_BGR2RGB), 1)
-                image = add_text_to_image(image, text)
-                image = Image.fromarray(image)
-                image = ImageTk.PhotoImage(image)
+                frame_counter = 0
 
-                if self.panel is None:
-                    self.panel = tki.Label(image=image)
-                    self.panel.image = image
-                    self.panel.pack(side="left", padx=1, pady=1)
-                else:
-                    self.panel.configure(image=image)
-                    self.panel.image = image
+            self.frame = self.vs.read()
+            self.frame = resize(self.frame, width=600)
+            image = cv2.flip(cv2.cvtColor(self.frame, cv2.COLOR_BGR2RGB), 1)
+            image = add_text_to_image(image, text)
+            image = Image.fromarray(image)
+            image = ImageTk.PhotoImage(image)
 
-                frame_counter = frame_counter + 1
-            
+            if self.panel is None:
+                self.panel = tki.Label(image=image)
+                self.panel.image = image
+                self.panel.pack(side="left", padx=1, pady=1)
+            else:
+                self.panel.configure(image=image)
+                self.panel.image = image
+
+            frame_counter = frame_counter + 1
+
     def onClose(self):
         self.stopEvent.set()
         self.vs.stop()
         self.root.quit()
 
-vs = VideoStream(src=0).start()
-time.sleep(2.0)
-wps = WorkPlaceScreening(vs)
-#wps.root.attributes("-fullscreen", True)
-wps.root.mainloop()
+
+if __name__ == "__main__":
+
+    vs = VideoStream(src=0).start()
+    time.sleep(2.0)
+    wps = WorkPlaceScreening(vs)
+    #wps.root.attributes("-fullscreen", True)
+    wps.root.mainloop()
